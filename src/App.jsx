2365b0863fa123379a5e7c32fbb0667f5daf9382
Matthew <mpstinis@gmail.com>
2020-01-01 22:38:41 -0500

    Initial setup of site structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Homepage from "./Homepage.jsx";
+
+class App extends Component {
+  render = () => {
+    return (
+      <div>
+        <Homepage />
+      </div>
+    );
+  };
+}
+
+export default App;
---
+++ src/Homepage.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class Homepage extends Component {
+  render = () => {
+    return (
+      <div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias
+          nesciunt perferendis corporis architecto optio soluta ea minus velit
+          cum accusantium dolorem sapiente culpa et at error, ad quas adipisci
+          rerum.
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores rerum
+          nemo nihil hic, architecto reprehenderit repudiandae reiciendis nam
+          magni facere nobis culpa esse explicabo ab saepe fuga exercitationem
+          quis sequi!
+        </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          odit sint soluta voluptatem saepe iure fuga blanditiis in non
+          perspiciatis magnam dolor voluptatum accusantium maiores, amet
+          cupiditate similique exercitationem ex.
+        </div>
+      </div>
+    );
+  };
+}
+
+export default Homepage;
