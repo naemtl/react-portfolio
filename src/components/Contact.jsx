1f5c8ecefe11fc0900ad3309b6c5a956ff2eab7b
Matthew <mpstinis@gmail.com>
2020-08-13 22:19:01 -0400

    test

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 // template_lg8ni0d4
- 
+// https://medium.com/@belktaylor12/creating-a-contact-form-for-your-react-portfolio-without-a-backend-dc1abaca820f 
 
 const Contact = () => {
     return (
