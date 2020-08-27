bc7ae3ee2b411bcd26944a4cd8d0da3620a6fab7
Matthew <mpstinis@gmail.com>
2020-08-27 13:41:11 -0400

    homepage styling, centering, line-height

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,12 +5,14 @@ const Homepage = () => {
     <div className="homepage-container">
       <div className="homepage-overlay">
         <div className="homepage-content">
-          <p className="homepage-bio">
-            A software engineer with a genuine interest in the hows and whys of programming. I strive to improve my knowledge and ability at any opportunity given to me.
-          </p>
-          <p className="homepage-bio">
-            Background in the arts, particularly music and sound design.
-          </p>
+          <div className="homepage-content-inner">
+            <p className="homepage-bio">
+              A software engineer with a genuine interest in the hows and whys of programming. I strive to improve my knowledge and ability at any opportunity given to me.
+            </p>
+            <p className="homepage-bio">
+              Background in the arts, particularly music and sound design.
+            </p>
+          </div>
         </div>
       </div>
     </div>
